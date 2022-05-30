99380c971dbf83db87ee5488cb3a8d69a6ec680f
이승빈 <iseungbin@ralph.local>
2022-05-30 14:55:54 +0900

    api 구현

---
+++ service/models/employee.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Information(models.Model):
+    employee_id = models.PositiveIntegerField(
+
+    )
+
+    name = models.CharField(
+        max_length=200
+    )
+
+    remain_dayd = models.PositiveIntegerField(
+
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
--- service/views.py
+++ service/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from service.models import *
+from .serializers import InformationSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def getData(request):
+     information = Information.objects.all()
+     serializer = InformationSerializer(information, many=True)
+     return Response(serializer.data)
+
+@api_view(['post'])
+def addPerson(request):
+     serializer = InformationSerializer(data=request.data)
+     if serializer.is_valid():
+          serializer.save()
+     return Response(serializer.data)
